4b49ba0fcc9a093fabad92d724eb2327dc2d1221
Artem <artemmz@inbox.ru>
2021-03-20 22:57:15 +0300

    init: add interective

--- virtual-piano/index.js
+++ virtual-piano/index.js
@@ -28,3 +28,35 @@ function play(audioBuffer, context) {
     source.connect(context.destination);
     source.start();
 }
+ ////////////////////My code
+const collection = document.querySelectorAll(".piano-key")
+
+// collection.forEach((elem) => {
+//     elem.addEventListener("click", (event) => {
+//         event.target.classList.add("active")
+//     })
+// });
+const addActive = (event) => {
+    event.target.classList.add("active");
+}
+const removeActive = (event) => {
+    event.target.classList.remove("active");
+}
+const startMouseover = (event) => {
+    event.target.classList.add("active")
+    collection.forEach((elem) => {
+        elem.addEventListener("mouseover", addActive)
+        elem.addEventListener("mouseout", removeActive)
+    });
+}
+
+const stoptMouseover = (event) => {
+    collection.forEach((elem) => {
+        elem.classList.remove("active");
+        elem.removeEventListener("mouseover", addActive)
+        elem.removeEventListener("mouseout", removeActive)
+    });
+}
+
+piano.addEventListener("mousedown", startMouseover);  
+piano.addEventListener("mouseup", stoptMouseover);
